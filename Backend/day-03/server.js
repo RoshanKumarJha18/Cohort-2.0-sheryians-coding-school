f467d064c33922a4738cf8fce6db189c7386cd97
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-02-22 10:25:17 +0530

    created a mini project notes backend

--- Backend/day-03/server.js
+++ Backend/day-03/server.js
@@ -1,24 +1,48 @@
-const express = require('express');
+const express = require("express");
 
-const app = express();// creating the server
+const app = express(); // creating the server
+
+//mini project to create , delete, update and get the notes from the postman using the express framework
 
 //creating notes from post method
-app.use(express.json()) // to parse the incoming request body as JSON
+app.use(express.json()); // to parse the incoming request body as JSON
 
-const notes =[];// creating an array to store the notes
-app.post('/notes',(req,res)=>{
-    notes.push(req.body); // adding the note to the array
-    res.json({
-         message:"Note added successfully"
-    })
-    
-})
-app.get('/notes',(req,res)=>{ // getting the notes from the array
-    res.json({
-        data:notes
-    })
-})
+const notes = []; // creating an array to store the notes
 
-app.listen(3000,()=>{ // starting the server on port 3000
-    console.log("Server is running on port 3000");
-})
+//Method POST to add the note to the array
+app.post("/notes", (req, res) => {
+  notes.push(req.body); // adding the note to the array
+  res.json({
+    message: "Note added successfully",
+  });
+});
+
+//Method DELETE to delete the note from the array
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.json({
+    message: "Note deleted successfully",
+  });
+});
+
+//Method PATCH to update the note from the array
+app.patch("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  let { title } = req.body;
+  notes[index].title = title;
+  res.json({
+    message: "Note updated successfully",
+  });
+});
+
+//Method GET to get the notes from the array
+app.get("/notes", (req, res) => {
+  // getting the notes from the array
+  res.json(notes);
+});
+
+app.listen(3000, () => {
+  // starting the server on port 3000
+  console.log("Server is running on port 3000");
+});
