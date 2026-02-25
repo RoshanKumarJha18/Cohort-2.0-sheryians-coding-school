3a401ff6a21f108bc3cf1d41f418b5a08a18f00f
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-02-25 21:29:41 +0530

    changed the mongodb link to env

--- Backend/day-04/src/db/db.js
+++ Backend/day-04/src/db/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
  // connecting database from db.js file to the server.js
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://roshanjha20041118_db_user:qd7SDU6ndELchrK3@cluster0.g8fvhrw.mongodb.net/cohort",
+      process.env.MONGODB_URI,
     )
     .then(() => {
       console.log("Connected to MongoDB");
