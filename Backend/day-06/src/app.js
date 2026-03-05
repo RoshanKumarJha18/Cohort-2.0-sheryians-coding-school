74799da681a7816f7998f762952ac7389987a183
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-03-05 10:56:30 +0530

    Add cookie-parser and implement JWT authentication in auth routes
    - Integrated cookie-parser middleware for handling cookies.
    - Added JWT token generation upon user registration.
    - Implemented user retrieval with token verification in the /users route.

--- Backend/day-06/src/app.js
+++ Backend/day-06/src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = require("./routes/auth.route");
+const cookies = require("cookie-parser");
 
 const app = express();
-app.use(express.json()); // this middleware is used for parsing the json data which is coming from the client side in the request body
-
+app.use(express.json());
+ // this middleware is used for parsing the json data which is coming from the client side in the request body
+app.use(cookies()); // this middleware is used for parsing the cookies which is coming from the client side in the request header   
 app.use("/auth", router); //this is middleare which says that the api routes is /auth and then it will go to auth.route.js file and check for the routes there
 
 module.exports = app;
--- Backend/day-06/src/routes/auth.route.js
+++ Backend/day-06/src/routes/auth.route.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const authModel = require('../models/auth.model');
+const jwt = require('jsonwebtoken');
+const cookies = require('cookie-parser');
 
 
 router.post('/register',async (req,res)=>{
     const data = req.body;
     const userauthModel = await authModel.create(data)
     console.log(userauthModel)
+    const token = jwt.sign({
+        id:userauthModel._id
+    },process.env.JWT_SECRET)
+
+    res.cookie("token",token)
+    
+    
     res.status(201).json({
         message:"User registered successfully",
-        data:userauthModel
+        data:userauthModel,
+        token:token
     })
 })
 
@@ -38,6 +48,31 @@ router.post('/login',async (req,res)=>{
    
 })
 
+router.get('/users',async(req,res)=>{
+    const {token} = req.cookies;
+    
+   try{
+     const decode = jwt.verify(token,process.env.JWT_SECRET);
+    const user = await authModel.findOne({
+        _id:decode.id
+    }).select("-password -__v")
+    if(!user){
+        return res.status(401).json({
+            message:"User not found"
+        })
+        }
+    
+      return res.status(200).json({
+            message:"User found",
+            data:user
+        })  
+    
+   }catch(err){
+    return res.status(401).json({
+        message:"Invalid token"
+    })
+   }
+})
 
 
 module.exports = router;
