deb3b50af1f5df186cc4c06dee7959ac696ef255
RoshanKumarJha18 <roshanjhajha76@gmail.com>
2026-03-10 18:00:38 +0530

    - Implemented generateCaption function in ai.service.js to generate captions for images using Google GenAI. - Updated post.controller.js to utilize the generateCaption function and return the generated caption in the response.

--- Backend/cohort_ai_Project/src/controllers/post.controller.js
+++ Backend/cohort_ai_Project/src/controllers/post.controller.js
@@ -1,7 +1,12 @@
 const postModel = require("../models/post.model");
+const generateCaption = require("../services/ai.service");
 
 const postController = async (req, res) => {
     const file = req.file;
+    const base64 = file.buffer.toString("base64"); 
+    const caption = await generateCaption(base64);
+    res.status(200).json({ caption });
+    
 }
 
 module.exports = postController;
